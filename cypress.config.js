c53e9f5573b9f699527930994ed6ec4b1d49b597
Elissdev <elissandra.dev@gmail.com>
2026-04-23 12:34:16 -0300

    ci: configura mochawesome e upload de artefatos no github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
